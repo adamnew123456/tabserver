1e99e3a288873122474ed17ffc3ddadd391634c1
adamnew123456 <adamnew123456@gmail.com>
2023-07-15 17:47:47 -0400

    Cleanup Java version and update for pagination changes

--- samples/TabServer.java
+++ samples/TabServer.java
@@ -7,7 +7,18 @@ import java.util.Base64;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+/// A tabserver client. Once started, it connects to a tabserver instance and
+/// executes its commands on the provided database connection.
 public class TabServer {
+  /// An error indicating that the server sent unexpected data over the
+  /// connection. This should never be handled, except to close the connection
+  /// immediately.
+  private static class ProtocolException extends Exception {
+    public ProtocolException(String reason) {
+      super(reason);
+    }
+  }
+
   private String _host;
   private int _port;
   private Connection _cnx;
@@ -28,7 +39,12 @@ public class TabServer {
 
     Class.forName(driverName);
     Connection cnx = DriverManager.getConnection(connectionString);
-    new TabServer(server, port, cnx).start();
+    try {
+      new TabServer(server, port, cnx).start();
+    } catch (ProtocolException err) {
+      System.err.println("Aborting due to protocol error: " + err.getMessage());
+      err.printStackTrace();
+    }
   }
 
   public TabServer(String host, int port, Connection cnx) {
@@ -37,42 +53,47 @@ public class TabServer {
     _cnx = cnx;
   }
 
+  /// Reads a line of base64-encoded data from the server and decodes it
   private String readData() throws IOException {
     String encodedBlob = readRaw();
     byte[] blob = Base64.getDecoder().decode(encodedBlob);
     return new String(blob, StandardCharsets.UTF_8);
   }
 
+  /// Reads a line of data from the server without decoding it
   private String readRaw() throws IOException {
     _peerSend.flush();
     return _peerRecv.nextLine();
   }
 
+  /// Encodes the data and sends it to the server
   private void sendData(String data) throws IOException {
     byte[] blob = data.getBytes(StandardCharsets.UTF_8);
     String encodedBlob = Base64.getEncoder().encodeToString(blob);
     sendRaw(encodedBlob);
   }
 
+  /// Sends the data to the server without any encoding
   private void sendRaw(String data) throws IOException {
     _peerSend.write(data);
     _peerSend.write('\n');
   }
 
+  /// Sends a list of columns and types to the server. Returns the number of
+  /// columns that were sent.
   private int sendMetadata(ResultSetMetaData metadata) throws IOException, SQLException {
-    String[] metadataLines = null;
-    int columnCount = 0;
-
-    // Metadata errors can only be reported up-front, so build the response first
-    columnCount = metadata.getColumnCount();
-    metadataLines = new String[2 * columnCount];
+    // Not a separate spot in the protocol for reporting metadata errors. This
+    // shouldn't normally fail, but if it does we want it to fail before we send
+    // any part of METADATA.
+    int columnCount = metadata.getColumnCount();
+    String[] metadataLines = new String[2 * columnCount];
     for (int i = 0; i < columnCount; i++) {
       metadataLines[2*i] = metadata.getColumnLabel(i + 1);
       metadataLines[2*i + 1] = metadata.getColumnTypeName(i + 1);
     }
 
     sendRaw("METADATA");
-    sendRaw("" + columnCount);
+    sendRaw(String.valueOf(columnCount));
     for (String line: metadataLines) {
       sendData(line);
     }
@@ -80,97 +101,120 @@ public class TabServer {
     return columnCount;
   }
 
-  private void executeQuery() throws IOException {
+  /// Sends a PAGE repsonse and gets the size of the next page. Returns 0 if the
+  /// server sends an ABORT.
+  private int getNextPageSize() throws IOException, ProtocolException {
+    sendRaw("PAGE");
+    String response = readRaw();
+    if (response.equals("MORE")) {
+      String rowCount = readRaw();
+      int rowsLeft;
+      try {
+        rowsLeft = Integer.parseInt(rowCount);
+      } catch (NumberFormatException err) {
+        throw new ProtocolException("Non-numeric page size: " + rowCount);
+      }
+
+      if (rowsLeft <= 0) {
+        throw new ProtocolException("Invalid page size: " + rowCount);
+      }
+
+      return rowsLeft;
+    } else if (response.equals("ABORT")) {
+      return 0;
+    } else {
+      throw new ProtocolException("Invalid response to PAGE command: " + response);
+    }
+  }
+
+  /// Sends rows to the server, pausing to prompt for MORE/ABORT as needed.
+  private void paginateQuery(ResultSet rs, int columnCount) throws IOException, ProtocolException, SQLException {
+    String[] rowdataLines = new String[columnCount];
+    int rowsLeft = 0;
+    while (rs.next()) {
+      if (rowsLeft == 0) {
+        rowsLeft = getNextPageSize();
+        if (rowsLeft == 0) break;
+      }
+
+      // Like METADATA, we have to buffer the row because we can't send an ERROR
+      // repsonse after we've sent the ROW response. An error in any column has
+      // to be reported up-front.
+      rowsLeft--;
+      for (int i = 0; i < columnCount; i++) {
+        Object value = rs.getObject(i + 1);
+
+        if (value == null) {
+          rowdataLines[i] = "<null>";
+        } else if (value instanceof byte[]) {
+          rowdataLines[i] = Base64.getEncoder().encodeToString((byte[]) value);
+        } else {
+          rowdataLines[i] = value.toString();
+        }
+      }
+
+      sendRaw("ROW");
+      for (String line: rowdataLines) {
+        sendData(line);
+      }
+    }
+
+    sendRaw("END");
+  }
+
+  /// Executes a query and sends back the affected rows, or the metadata and the
+  /// row data.
+  private void executeQuery() throws IOException, ProtocolException, SQLException {
     String query = readData();
 
-    Statement stmt = null;
-    ResultSet rs = null;
-
-    try {
-      stmt = _cnx.createStatement();
+    try (Statement stmt = _cnx.createStatement()) {
       stmt.execute(query);
 
-      rs = stmt.getResultSet();
-      int affected = stmt.getUpdateCount();
+      try (ResultSet rs = stmt.getResultSet()) {
+        if (rs == null) {
+          sendRaw("AFFECTED");
+          sendRaw(String.valueOf(stmt.getUpdateCount()));
+          return;
+        }
 
-      if (rs == null) {
-        sendRaw("AFFECTED");
-        sendRaw("" + affected);
-        return;
+        int columnCount = sendMetadata(rs.getMetaData());
+        paginateQuery(rs, columnCount);
       }
-
-      int columnCount = sendMetadata(rs.getMetaData());
-      String[] rowdataLines = new String[columnCount];
-
-      int pageLeft = 25;
-      while (rs.next()) {
-        if (pageLeft == 0) {
-          sendRaw("PAGE");
-          String response = readRaw();
-          if (response.equals("MORE")) {
-            pageLeft = 25;
-          } else if (response.equals("ABORT")) {
-            break;
-          }
-        }
-
-        // Same as before, row errors can only be emitted on a per-row basis
-        pageLeft--;
-        for (int i = 0; i < columnCount; i++) {
-          Object value = rs.getObject(i + 1);
-
-          if (value == null) {
-            rowdataLines[i] = "<null>";
-          } else if (value instanceof byte[]) {
-            rowdataLines[i] = Base64.getEncoder().encodeToString((byte[]) value);
-          } else {
-            rowdataLines[i] = value.toString();
-          }
-        }
-
-        sendRaw("ROW");
-        for (String line: rowdataLines) {
-          sendData(line);
-        }
-      }
-
-      sendRaw("END");
     } catch (SQLException err) {
       sendRaw("ERROR");
       sendData(err.getMessage());
-    } finally {
-      if (stmt != null) try { stmt.close(); } catch (SQLException ignore) { }
-      if (rs != null) try { rs.close(); } catch (SQLException ignore) { }
     }
   }
 
+  /// Prepares a query and reports its metadata.
   private void prepareQuery() throws IOException {
     String query = readData();
 
-    PreparedStatement stmt = null;
-    try {
-      stmt = _cnx.prepareStatement(query);
-      sendMetadata(stmt.getMetaData());
+    try (PreparedStatement stmt = _cnx.prepareStatement(query)) {
+      ResultSetMetaData prepMetadata = stmt.getMetaData();
+      if (prepMetadata == null) {
+        sendRaw("AFFECTED");
+        sendRaw("0");
+      } else {
+        sendMetadata(prepMetadata);
+      }
     } catch (SQLException err) {
       sendRaw("ERROR");
-      sendData("Could not prepare: " + err.getMessage());
-    } finally {
-      if (stmt != null) try { stmt.close(); } catch (SQLException ignore) { }
+      sendData(err.getMessage());
     }
   }
 
-  public void start() throws IOException, SQLException {
-    Socket peer = null;
-    try {
-      peer = new Socket(_host, _port);
-      _peerRecv = new Scanner(new InputStreamReader(peer.getInputStream(), StandardCharsets.US_ASCII));
-      _peerSend = new BufferedWriter(new OutputStreamWriter(peer.getOutputStream(), StandardCharsets.US_ASCII));
+  /// Accepts a single connection from the server and executes its commands
+  /// until it disconnects.
+  public void start() throws IOException, ProtocolException, SQLException {
+    try (Socket peer = new Socket(_host, _port)) {
+      _peerRecv = new Scanner(new InputStreamReader(peer.getInputStream(), StandardCharsets.UTF_8));
+      _peerSend = new BufferedWriter(new OutputStreamWriter(peer.getOutputStream(), StandardCharsets.UTF_8));
 
       DatabaseMetaData dbmd = _cnx.getMetaData();
       String identity = dbmd.getDriverName() + " " + dbmd.getDriverVersion();
       sendRaw("HELLO");
       sendData(identity);
-      _peerSend.flush();
 
       while (true) {
         String command;
@@ -182,14 +226,12 @@ public class TabServer {
           } else if (command.equals("PREPARE")) {
             prepareQuery();
           } else {
-            break;
+            throw new ProtocolException("Invalid command: " + command);
           }
         } catch (NoSuchElementException err) {
           break;
         }
       }
-    } finally {
-      if (peer != null) peer.close();
     }
   }
 }

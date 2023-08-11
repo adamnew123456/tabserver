5c835356541006d9c1a0c76a46a78e0895a62e5b
adamnew123456 <adamnew123456@gmail.com>
2023-08-11 12:28:07 -0400

    Add benchmarking suite
    The initial benchmarks are meant to cover WebSocket support, both in
    terms of building server frames as well as parsing client frames

---
+++ broker/brokerbench/Program.cs
@@ -0,0 +1,126 @@
+﻿// -*- mode: csharp; fill-column: 100 -*-
+using brokerlib;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+namespace brokerlib.benchmark;
+
+public class ClientFrameParser
+{
+    [Params(1)]
+    public int Frames { get; set; }
+
+    [Params(4096)]
+    public int ChunkSize { get; set; }
+
+    [Params(0, 100, 1000)]
+    public int DataSize { get; set; }
+
+    private byte[] FrameData;
+
+    [GlobalSetup]
+    public void BuildMessageBuffer()
+    {
+        var data = new byte[DataSize];
+        Random.Shared.NextBytes(data);
+
+        var frame = new WebSocketFrame()
+        {
+            IsLastFragment = true,
+            OpCode = MessageType.Binary,
+            Mask = new byte[] { 1, 2, 3, 4 },
+            Payload = data.Length,
+        };
+        FrameData = frame.ToArray(data);
+    }
+
+	/// Chunks a buffer into pieces with the given maximum size.
+	private IEnumerable<Memory<byte>> ChunkBuffer(byte[] buffer, int chunkSize)
+	{
+		var offset = 0;
+		while (offset < buffer.Length)
+		{
+			var thisChunk = Math.Min(buffer.Length - offset, chunkSize);
+			yield return new Memory<byte>(buffer, offset, thisChunk);
+			offset += thisChunk;
+		}
+	}
+
+    private void ParserCallback(WebSocketMessage message)
+    {
+    }
+
+    [Benchmark]
+    public void ParseFrames()
+    {
+        for (var i = 0; i < Frames; i++)
+        {
+            using (var parser = new WebSocketClientParser())
+            {
+                var buffer = parser.RentFeedBuffer();
+                foreach (var chunk in ChunkBuffer(FrameData, ChunkSize))
+                {
+                    chunk.CopyTo(buffer);
+                    parser.Feed(chunk.Length, ParserCallback);
+                }
+            }
+        }
+    }
+}
+
+public class FrameBuilding
+{
+    [Params(1)]
+    public int Frames { get; set; }
+
+    [Params(0, 100, 1000)]
+    public int DataSize { get; set; }
+
+    private WebSocketFrame Frame;
+    private byte[] Buffer;
+
+    [GlobalSetup]
+    public void BuildMessageBuffer()
+    {
+        var data = new byte[DataSize];
+        Random.Shared.NextBytes(data);
+
+        Frame = new WebSocketFrame()
+        {
+            IsLastFragment = true,
+            OpCode = MessageType.Binary,
+            Mask = new byte[] { 1, 2, 3, 4 },
+            Payload = data.Length,
+        };
+        Buffer = new byte[Frame.RequiredCapacity()];
+    }
+
+    [Benchmark(Baseline=true)]
+    public void FillFrameData()
+    {
+        var dataSpan = Frame.WriteHeaderTo(new Span<byte>(Buffer));
+        for (var i = 0; i < Frames; i++)
+        {
+            dataSpan.Fill(42);
+        }
+    }
+
+    [Benchmark]
+    public void BuildFrames()
+    {
+        for (var i = 0; i < Frames; i++)
+        {
+            var dataSpan = Frame.WriteHeaderTo(new Span<byte>(Buffer));
+            dataSpan.Fill(42);
+            Frame.MaskPayloadInBuffer(dataSpan);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
